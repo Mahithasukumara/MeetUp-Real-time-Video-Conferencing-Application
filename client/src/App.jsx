ab1863c05d790dd3f1fa700ff3a9777cd9dcfb0c
Mahithasukumara <mahissk2005@gmail.com>
2025-08-11 15:01:21 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+    <div className='flex items-center justify-center min-h-screen'>
+    <p className='text-5xl font-bold text-blue-500'>MeetUp</p>
+    </div>
+    </>
+  )
+}
+
+export default App
