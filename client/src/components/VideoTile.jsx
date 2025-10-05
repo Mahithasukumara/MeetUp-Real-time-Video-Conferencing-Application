6bbe331f2a434a887b59a42eecb3b7b3f4788d2f
lohit <lohitvenkatasai2004@gmail.com>
2025-10-06 00:01:21 +0530

    worked on meet page in client and mediasoup service in server

--- client/src/components/VideoTile.jsx
+++ client/src/components/VideoTile.jsx
@@ -1,7 +1,32 @@
 import React from "react";
-
+import useMeetStore from "../store/meetStore.js";
+import useIcons from "../constants/Icons.jsx";
+import { BsMicFill, BsMicMute } from "react-icons/bs";
 const VideoTile = ({ className }) => {
-  return <div className={`${className} aspect-video`}></div>;
+  // if the participent is not a producer
+  // return (
+  //   <div
+  //     className={`${className} bg-[#27391C]  border rounded-lg aspect-video p-2 flex items-center`}
+  //   >
+  //     <span>user name</span>
+  //   </div>
+  // );
+  return (
+    <div
+      className={`${className} rounded-lg border aspect-video p-2 flex flex-col`}
+    >
+      <div className="flex justify-between">
+        <span className="flex justify-center items-center">user name</span>
+        <span className="flex justify-center items-center border p-2 rounded-full">
+          <BsMicMute className=" h-4 w-4" />
+        </span>
+      </div>
+      <div className=" flex-grow flex justify-center items-center ">
+        <span className="border p-3 rounded-full">U1</span>
+      </div>
+    </div>
+  );
+  // return <div className={`${className} rounded-lg border aspect-video`}></div>;
 };
 
 export default VideoTile;
--- client/src/store/controlBarStore.js
+++ client/src/store/controlBarStore.js
@@ -3,12 +3,19 @@ import { create } from "zustand";
 const useStore = create((set) => ({
   isMicOn: false,
   isCamOn: false,
+  isPresenting: false,
   isSideBarOpen: false,
+  isParticipentPanelOpen: false,
 
   ToggleMic: () => set((state) => ({ isMicOn: !state.isMicOn })),
+  ToggleParticipentPanel: () =>
+    set((state) => ({
+      isParticipentPanelOpen: !state.isParticipentPanelOpen,
+    })),
 
   ToggleCam: () => set((state) => ({ isCamOn: !state.isCamOn })),
-
+  TogglePresent: () => set((state) => ({ isPresenting: !state.isPresenting })),
   ToggleSideBar: () =>
     set((state) => ({ isSideBarOpen: !state.isSideBarOpen })),
 }));
+export default useStore;
